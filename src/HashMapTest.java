b289d8b5a1e034dd683e1b8a95520ffe9e356027
Arunkumar <arunkumarbs97@gmail.com>
2022-05-25 10:31:55 +0530

    Commit

---
+++ src/HashMapTest.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapTest
+{
+    public static void main(String[] args)
+    {
+
+        String str = "Paranoids are not paranoids because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+        Map<String, Integer> hashMap = new HashMap<>();
+
+        String[] words = str.split(" ");
+        for (String word : words) {
+            Integer integer = hashMap.get(word);
+
+            if (integer == null)
+                hashMap.put(word, 1);
+
+            else {
+                hashMap.put(word, integer + 1);
+            }
+        }
+        System.out.println(hashMap);
+    }
+}
